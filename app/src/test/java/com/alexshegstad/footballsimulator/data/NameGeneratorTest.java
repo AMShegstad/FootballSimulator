5be866c8563b889d46434c757c5797d9f7a80788
Alexander Shegstad <aShegstad@my365.Bellevue.edu>
2026-08-13 01:45:04 -0500

    commented out unused import 'import org.junit.jupiter.api.Test'

--- app/src/test/java/com/alexshegstad/footballsimulator/data/NameGeneratorTest.java
+++ app/src/test/java/com/alexshegstad/footballsimulator/data/NameGeneratorTest.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
